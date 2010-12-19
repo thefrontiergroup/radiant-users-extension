0dff7e30022a189f4eaf17f3c71fb69e9d15f7ad
Dirk Kelly <dk@dirkkelly.com>
2010-12-19 21:00:39 +0800

    updating the helper

--- lib/users/tags/helpers.rb
+++ lib/users/tags/helpers.rb
@@ -2,14 +2,12 @@ module Users
   module Tags
     class Helpers
       class << self
-        include ActionView::Helpers::NumberHelper
         
         def current_user(tag)
           begin
-            user_id = tag.locals.page.request.session['user_id']
-            User.find(user_id)
+            User.find(tag.locals.page.request.session['user_id'])
           rescue
-            return false
+            nil
           end
         end
         
