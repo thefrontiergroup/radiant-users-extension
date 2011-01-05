1877e921c656fa77bb96822d54f5305e517e0944
Dirk Kelly <dk@dirkkelly.com>
2011-01-05 13:45:57 +0800

    Updated the user model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe User do
+  
+  dataset :devise_users
+  
+  it { should have_default_scope(:order => 'name') }
+  
+  it { should belong_to(:created_by).class_name('User') }
+  it { should belong_to(:updated_by).class_name('User') }
+  
+  describe "self.unprotected_attributes" do
+    it "should be an array of [:name, :email, :login, :password, :password_confirmation, :locale]" do
+      # Make sure we clean up after anything set in another spec
+      User.instance_variable_set(:@unprotected_attributes, nil)
+      User.unprotected_attributes.should == [:name, :email, :username, :login, :password, :password_confirmation, :locale]
+    end
+  end
+  describe "self.unprotected_attributes=" do
+    it "should set the @@unprotected_attributes variable to the given array" do
+      User.unprotected_attributes = [:password, :email, :other]
+      User.unprotected_attributes.should == [:password, :email, :other]
+    end
+  end
+  
+  describe "self.find_for_authentication" do
+    it "should find by username" do
+      @user = User.find_for_authentication({:login => users(:admin).username})
+      @user.should === users(:admin)
+    end
+  end
+  
+end
+
+describe User, "roles" do
+  
+  dataset :devise_users
+  
+  it "should not have a non-existent role" do
+    users(:existing).has_role?(:foo).should be_false
+  end
+  
+  it "should not have a role for which the corresponding method returns false" do
+    users(:existing).has_role?(:designer).should be_false
+    users(:existing).has_role?(:admin).should be_false
+  end
+  
+  it "should have a role for which the corresponding method returns true" do
+    users(:designer).has_role?(:designer).should be_true
+    users(:admin).has_role?(:admin).should be_true
+  end
+end
