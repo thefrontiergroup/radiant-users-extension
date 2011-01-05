df0e0148c8ff5309e9f788e3a4c32b4dccd3cf0b
Dirk Kelly <dk@dirkkelly.com>
2011-01-05 23:28:01 +0800

    reimplementing the authorize methods, these, I realise are completely seperate to authenticate.

--- lib/login_system.rb
+++ lib/login_system.rb
@@ -2,13 +2,34 @@ module LoginSystem
   
   def self.included(base)
     base.extend ClassMethods
+    base.class_eval do
+      prepend_before_filter :authorize
+    end
   end
   
-  # Empty
+  def authorize
+    action = action_name.to_s.intern
+    if user_has_access_to_action?(action)
+      true
+    else
+      permissions = self.class.controller_permissions[action]
+      flash[:error] = permissions[:denied_message] || 'Access denied.'
+      respond_to do |format|
+        format.html { redirect_to(permissions[:denied_url] || { :action => :index }) }
+        format.any(:xml, :json) { head :forbidden }
+      end
+      false
+    end
+  end
+  
+  def user_has_access_to_action?(action)
+    self.class.user_has_access_to_action?(current_user, action, self)
+  end
   
   module ClassMethods
     def no_login_required
       skip_before_filter :authenticate_user!
+      skip_before_filter :authorize
     end
     
     def only_allow_access_to(*args)
