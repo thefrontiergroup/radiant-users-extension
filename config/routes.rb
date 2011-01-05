f1a82069c225a6c7a1c8100e1fb905b9ded7022d
Dirk Kelly <dk@dirkkelly.com>
2011-01-05 13:10:27 +0800

    The standard set of devise routes, with overides for login/logout and their admin counterparts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
-  # map.namespace :admin, :member => { :remove => :get } do |admin|
-  #   admin.resources :users
-  # end
+  
+  map.devise_for                :users,         :path_names => { :sign_in => 'login', :sign_out => 'logout' }
+  map.new_user_session          'login',        :controller => 'sessions',      :action => 'new',     :conditions => { :method => :get  }
+  map.old_user_session          'admin/login',  :controller => 'sessions',      :action => 'new',     :conditions => { :method => :get  }
+  map.user_session              'login',        :controller => 'sessions',      :action => 'create',  :conditions => { :method => :post }
+  map.destroy_user_session      'logout',       :controller => 'sessions',      :action => 'destroy', :conditions => { :method => :get  }
+  map.old_destroy_user_session  'admin/logout', :controller => 'sessions',      :action => 'destroy', :conditions => { :method => :get  }
+  
+  map.user_root_path            'admin',        :controller => 'admin/welcome', :action => 'index'
+  # This can redirect to anything by overiding ApplicationController#default_admin_path
+  
 end
