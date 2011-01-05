57d5daa1727f8b1a49b760398a051af73fab306b
Dirk Kelly <dk@dirkkelly.com>
2011-01-05 13:12:11 +0800

    The new devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ActiveRecord::Base
+  
+  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :token_authenticatable, :validatable
+  
+  attr_accessor :login
+  
+  def login; username; end
+  
+  # Default Order
+  default_scope           :order => 'name'
+  
+  # Associations
+  belongs_to :created_by, :class_name => 'User'
+  belongs_to :updated_by, :class_name => 'User'
+  
+  def has_role?(role)
+    respond_to?("#{role}?") && send("#{role}?")
+  end
+  
+  class << self
+    def unprotected_attributes
+      @unprotected_attributes ||= [:name, :email, :username, :login, :password, :password_confirmation, :locale]
+    end
+    
+    def unprotected_attributes=(array)
+      @unprotected_attributes = array.map{|att| att.to_sym }
+    end
+    
+    def find_for_authentication(conditions)
+      login = conditions.delete(:login)
+      find(:first, :conditions => ["username = ? OR email = ?", login, login])
+    end
+  end
+  
+end
