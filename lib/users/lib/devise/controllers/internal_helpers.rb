41be953322a52ffc9083f673d7ccf1aedc49eab6
Dirk Kelly <dk@dirkkelly.com>
2011-01-06 12:02:39 +0800

    Properly translating the flash messages to radiant error and notice

--- lib/users/lib/devise/controllers/internal_helpers.rb
+++ lib/users/lib/devise/controllers/internal_helpers.rb
@@ -9,9 +9,10 @@ module Users
             base.class_eval do
               
               def set_flash_message(key, kind, now=false)
-                kind = [:invalid, :alert].include?(kind) ? :error : kind
+                type = [:invalid, :alert, :unauthenticated].include?(kind) ? :error : kind
+                type = [:error].include?(type) ? type : :notice
                 
-                flash[kind] = I18n.t(:"#{resource_name}.#{kind}", 
+                flash[type] = I18n.t(:"#{kind}", 
                   :scope => [:devise, controller_name.to_sym], :default => kind)
               end
               
