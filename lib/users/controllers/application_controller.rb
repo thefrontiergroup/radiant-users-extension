a3fd65b38820e9f49bba6ed195bd9570f7d97daf
Dirk Kelly <dk@dirkkelly.com>
2011-01-06 13:48:15 +0800

    Don't try to retrieve a custom path if the user isn't signed in, if they don't have a custom path redirect to the root_path

--- lib/users/controllers/application_controller.rb
+++ lib/users/controllers/application_controller.rb
@@ -8,10 +8,11 @@ module Users
           private
           
           def default_admin_path
-            default_user_path || admin_pages_path
+            default_user_path || root_path
           end
           
           def default_user_path
+            return false unless user_signed_in?
             Radiant::Config["defaults.path.#{current_user.class_name.downcase}"]
           end
           
