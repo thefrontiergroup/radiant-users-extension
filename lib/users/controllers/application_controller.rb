2a8cf347766c5e5dcf8f5a475903349c58fc9634
Dirk Kelly <dk@dirkkelly.com>
2011-01-13 14:55:06 +0800

    support for taking a moderator to pages

--- lib/users/controllers/application_controller.rb
+++ lib/users/controllers/application_controller.rb
@@ -13,6 +13,7 @@ module Users
           
           def default_user_path
             return false unless user_signed_in?
+            return '/admin/pages' if user_has_access_to_action?(:index)
             Radiant::Config["defaults.path.#{current_user.class_name.downcase}"]
           end
           
