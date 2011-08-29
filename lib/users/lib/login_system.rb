99eaebf30ab1bc2ae414fc692938e8486a40ee3f
Mario Visic <mario@mariovisic.com>
2011-08-29 15:07:02 +0800

    Added customer impersonation

--- lib/users/lib/login_system.rb
+++ lib/users/lib/login_system.rb
@@ -1,48 +1,62 @@
 module Users
   module Lib
     module LoginSystem
-      
+
       def self.included(base)
         base.class_eval do
           protected
-            alias_method :authorize_radiant, :authorize
-            alias_method :current_user_radiant, :current_user
-            
-            def authorize
-              if user_has_backend_access?
-                authorize_radiant
-              else
-                path = Radiant::Config["scoped.#{current_user.access}.welcome"]
-                path = "/#{path}".gsub('//','/')
-                
-                respond_to do |format|
-                  format.any(:html, :xml, :json) { redirect_to(path) }
-                end
+          alias_method :authorize_radiant, :authorize
+          alias_method :current_user_radiant, :current_user
+
+          def authorize
+            if user_has_backend_access?
+              authorize_radiant
+            else
+              path = Radiant::Config["scoped.#{current_user.access}.welcome"]
+              path = "/#{path}".gsub('//','/')
+
+              respond_to do |format|
+                format.any(:html, :xml, :json) { redirect_to(path) }
               end
             end
+          end
 
-            def user_has_backend_access?
-              current_user.access.blank?
-            end
-            
-            def current_user
-              @current_user ||= (current_user_radiant || login_from_api_key)
-            end
+          def user_has_backend_access?
+            current_user.access.blank?
+          end
 
-            def login_from_api_key
-              result = nil
-              if params[:api_key].present?
-                result = User.find_by_api_key(params[:api_key])
-              else
-                authenticate_with_http_basic do |api_key,_|
-                  result = User.find_by_api_key(api_key)
-                end
+          def current_impersonated_customer
+            if customer_id = session[:impersonated_customer_id]
+              ShopCustomer.find_by_id(customer_id)
+            end
+          end
+
+          def impersonating_customer?
+            current_impersonated_customer.present?
+          end
+
+          def impersonate_customer(customer_id)
+            session[:impersonated_customer_id] = customer_id
+          end
+
+          def current_user
+            @current_user ||= (current_impersonated_customer || current_user_radiant || login_from_api_key)
+          end
+
+          def login_from_api_key
+            result = nil
+            if params[:api_key].present?
+              result = User.find_by_api_key(params[:api_key])
+            else
+              authenticate_with_http_basic do |api_key,_|
+                result = User.find_by_api_key(api_key)
               end
-              result
-            end 
+            end
+            result
+          end 
         end
       end
-      
+
     end
   end
 end
--- lib/users/tags/helpers.rb
+++ lib/users/tags/helpers.rb
@@ -1,19 +1,24 @@
 module Users
   module Tags
-    class Helpers
-      class << self
-        include ActionView::Helpers::NumberHelper
-        
-        def current_user(tag)
-          begin
-            user_id = tag.locals.page.request.session['user_id']
-            User.find(user_id)
-          rescue
-            return false
-          end
+    module Helpers
+      include ActionView::Helpers::NumberHelper
+
+      def current_logged_in_user
+        if user_id = tag.locals.page.request.session[:user_id]
+          User.find_by_id(user_id)
         end
-        
       end
+
+      def current_impersonated_customer
+        if customer_id = request.session[:impersonated_customer_id]
+          ShopCustomer.find_by_id(customer_id)
+        end
+      end
+
+      def current_user(tag)
+        current_impersonated_customer || current_logged_in_user
+      end
+
     end
   end
 end
